52efa9aaf2c8a46554d86ee30dd09aae9b5cc9f4
尾崎健太 <kentaojapi@cool-MacBook-Pro.local>
2019-03-03 23:07:54 +0900

    add js libs

---
+++ js/bass.js
@@ -0,0 +1,51 @@
+// fade in out
+$(function(){
+	$('body').fadeMover();
+});
+
+// menu hover func
+$(function(){
+	$('#nav a').hover(function(){
+		$(this).css("color", "black");
+	}, function(){
+		$(this).css("color", "#4c4c4c");
+		});
+});
+
+// fixed menu
+$(function() {
+  var $win = $(window),
+      $main = $('.container'),
+      $nav = $('#nav'),
+      navPos = $nav.offset().top,
+      fixedClass = 'is-fixed';
+  $win.on('load scroll', function() {
+    var value = $(this).scrollTop();
+    if ( value > navPos ) {
+      $nav.addClass(fixedClass);
+    } else {
+      $nav.removeClass(fixedClass);
+    }
+  });
+});
+
+// smooth scroll
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - 20;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
+
+// lazy load
+$(function(){
+	$('.works-img').lazyload({
+		effect: 'fadeIn',
+		effectspeed: 1000,
+		threshold: 300
+	});
+});
